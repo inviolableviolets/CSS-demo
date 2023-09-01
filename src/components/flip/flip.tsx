3cf4c07a70ad086f27e510463b13947b0b370aa5
Sergio Mata <sergiomataherrero@gmail.com>
2023-09-01 09:17:14 +0200

    Class 3

--- src/components/flip/flip.tsx
+++ src/components/flip/flip.tsx
@@ -3,14 +3,14 @@ import style from './flip.module.scss';
 export default function Flip() {
   return (
     <a className={style['flip-container']} href="#header" id="down">
-      <div className={style.card}>
+      <main className={style.card}>
         <div className={style.front}>
           <p>↑</p>
         </div>
         <div className={style.back}>
           <p>Go back up</p>
         </div>
-      </div>
+      </main>
     </a>
   );
 }
